40f57cc7fc18c8c56b4e71569f29b88273f8bbd9
molinaro karine <molinaro.karine@hotmail.com>
2023-07-23 08:07:24 +0200

    Récuper le conteneur du slide

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// On récupère le conteneur principal du diaporama
+const diapo = document.querySelector("#banner");
+console.log(diapo);
+
+// On récupère les deux flèches
+let next = document.querySelector(".arrow_left");
+let prev = document.querySelector(".arrow_right");
+console.log(next);
