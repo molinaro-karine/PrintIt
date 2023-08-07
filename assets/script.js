150dc33770cfc7ff5abf9b2aa488a8334ae0ce87
molinaro karine <molinaro.karine@hotmail.com>
2023-08-07 13:38:00 +0200

    adding comments

--- assets/script.js
+++ assets/script.js
@@ -20,14 +20,13 @@ const slides = [
 const slideContainer = document.querySelector(".banner-img");
 const sliderText = document.querySelector("#banner > p");
 
-console.log(sliderText);
-
 let slideCounter = 0;
 
 const btnLeft = document.querySelector(".arrow_left");
 const btnRight = document.querySelector(".arrow_right");
-console.log(btnRight);
 
+/* The code is adding an event listener to the `btnRight` element, which represents the right arrow
+button in a slideshow. When the button is clicked, the function is executed. */
 btnRight.addEventListener("click", function () {
   if (slideCounter === slides.length - 1) {
     slideCounter = -1;
@@ -38,6 +37,8 @@ btnRight.addEventListener("click", function () {
   updatePosition();
 });
 
+/* The code is adding an event listener to the `btnLeft` element, which represents the left arrow
+button in a slideshow. When the button is clicked, the function is executed. */
 btnLeft.addEventListener("click", function () {
   if (slideCounter === 0) {
     slideCounter = slides.length;
@@ -49,6 +50,9 @@ btnLeft.addEventListener("click", function () {
   updatePosition();
 });
 
+/**
+ * The function updates the position of dots by adding and removing a "selected" class.
+ */
 function updatePosition() {
   dots.forEach((dot) => {
     dot.classList.remove("selected");
@@ -57,6 +61,7 @@ function updatePosition() {
   dots[slideCounter].classList.add("selected");
 }
 
+/* This code is creating and adding dots to the dotContainer element. */
 const dotContainer = document.querySelector(".dots");
 slides.forEach((_slide) => {
   const dot = document.createElement("div");
@@ -67,9 +72,11 @@ slides.forEach((_slide) => {
 const dots = document.querySelectorAll(".dot");
 dots[slideCounter].classList.add("selected");
 
+/* The code is adding an event listener to each dot element in the dots array. When a dot is clicked,
+the function is executed. Inside the function, the slideCounter variable is updated with the index
+of the clicked dot.*/
 dots.forEach((dot, index) => {
   dot.addEventListener("click", function () {
-    console.log(dot);
     slideCounter = index;
   });
 });
